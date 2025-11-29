6fff5c11141870cdcbaeb2da740d8e4f8064082d
hirenmario <hf284@live.mdx.ac.uk>
2025-11-29 19:41:59 +0400

    week 10

--- week 10/chatgpt_basic.py
+++ week 10/chatgpt_basic.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 # Initialize the OpenAI client with your API key
-client = OpenAI(api_key='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')
+client = OpenAI(api_key='')
 # Make a simple API call
 completion = client.chat.completions.create(
     model="gpt-4.1-mini",
