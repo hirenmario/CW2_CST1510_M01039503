f6c1af3449222b68787039d795361c605e3391c2
hirenmario <hf284@live.mdx.ac.uk>
2025-11-29 19:55:51 +0400

    week10 gitignore key

--- week 10/chatgpt_basic.py
+++ week 10/chatgpt_basic.py
@@ -1,15 +1,18 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-# Initialize the OpenAI client with your API key
-client = OpenAI(api_key='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')
+load_dotenv()
+
+client = OpenAI() 
+
 # Make a simple API call
 completion = client.chat.completions.create(
     model="gpt-4.1-mini",
     messages=[
-     {"role": "system", "content": "You are a helpful assistant."},
-     {"role": "user", "content": "Hello! What is AI?"}
-]
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": "Hello! What is AI?"}
+    ]
 )
 # Print the response
-
 print(completion.choices[0].message.content)
